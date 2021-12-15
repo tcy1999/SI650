2dff3a49fb1c0dba0b9dbf7f59aca033e33aa8af
tcy1999 <tcy9902@outlook.com>
2021-12-14 20:14:29 -0500

    Another deploy attempt with websocket

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_socketio import SocketIO, emit
 from online_tfidf_bert import IR_bert
 from csv2html import csv2html
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
 tag_file = 'tag_10_tfidf_2000.csv'
 output_file = 'out.csv'
 
@@ -14,9 +16,14 @@ def index():
 def results():
     if request.method == 'POST':
         query = request.form['query']
-        f_df = IR_bert(tag_file, query)
-        f_df.to_csv(output_file, index=False)
-        results, count = csv2html(query)
-        return render_template('results.html', query=query, results=results, count=count)
+        return render_template('results.html', query=query)
     else:
         return redirect(url_for('index'), code=302)
+
+@socketio.on('my event')
+def generateResults(message):
+    query = message['query']
+    f_df = IR_bert(tag_file, query)
+    f_df.to_csv(output_file, index=False)
+    results, count = csv2html(query)
+    emit('render', {'results': results, 'count': count})
