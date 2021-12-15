db4a5c48dcc57fe181fb170eaf10881bab6a66bc
tcy1999 <tcy9902@outlook.com>
2021-12-15 00:11:58 -0500

    Add loading

--- flask_app.py
+++ flask_app.py
@@ -3,20 +3,17 @@ from online_tfidf_bert import IR_bert
 from csv2html import csv2html
 
 app = Flask(__name__)
-tag_file = '/home/tcy1999/mysite/tag_10_tfidf_2000.csv'
-output_file = '/home/tcy1999/mysite/out.csv'
+tag_file = 'tag_10_tfidf_2000.csv'
+output_file = 'out.csv'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/results', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def results():
-    if request.method == 'POST':
-        query = request.form['query']
-        f_df = IR_bert(tag_file, query)
-        f_df.to_csv(output_file, index=False)
-        results, count = csv2html(query)
-        return render_template('results.html', query=query, results=results, count=count)
-    else:
-        return redirect(url_for('index'), code=302)
+    query = request.form['query']
+    f_df = IR_bert(tag_file, query)
+    f_df.to_csv(output_file, index=False)
+    results, count = csv2html(query)
+    return render_template('results.html', query=query, results=results, count=count)
